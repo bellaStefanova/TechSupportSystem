61d2eb09c4735a0566b4b2356015e8db6ed1e0c3
BellaStefanova <bellastefanova7@gmail.com>
2024-04-06 00:13:59 +0300

    update sh

--- TechSupportSystem/TechSupportSystem/startup.sh
+++ TechSupportSystem/TechSupportSystem/startup.sh
@@ -1,2 +1,16 @@
-python manage.py qcluster &
-gunicorn --bind=0.0.0.0 --timeout 600 TechSupportSystem.wsgi --access-logfile '-' --error-logfile '-'
+# python manage.py qcluster &
+# gunicorn --bind=0.0.0.0 --timeout 600 TechSupportSystem.wsgi --access-logfile '-' --error-logfile '-'
+
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+
+python manage.py migrate
