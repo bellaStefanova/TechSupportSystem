dfa48ae7c172e98c68b43cee7893fc713fbdadcb
BellaStefanova <bellastefanova7@gmail.com>
2024-04-06 23:26:25 +0300

    testing

---
+++ startup2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Start sshd"
+/usr/sbin/sshd
+# python manage.py qcluster &
+
+gunicorn --bind=0.0.0.0 --timeout 600 TechSupportSystem.wsgi --access-logfile '-' --error-logfile '-'
+
