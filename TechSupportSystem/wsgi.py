d233402f4a7ac6d3ce6e5f7da566f1982a335e6e
BellaStefanova <bellastefanova7@gmail.com>
2024-04-06 13:45:24 +0300

    testing

--- TechSupportSystem/wsgi.py
+++ TechSupportSystem/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 print(os.environ)
-if 'DBHOST' in os.environ:
+if os.environ.get('DBHOST') != None:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechSupportSystem.deployment')
 else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechSupportSystem.settings')
