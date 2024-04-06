68e14690b1881908f0dbc6a3e3e70a44aa69005f
BellaStefanova <bellastefanova7@gmail.com>
2024-04-06 09:06:14 +0300

    Temp settings usage

--- TechSupportSystem/wsgi.py
+++ TechSupportSystem/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'TechSupportSystem.deployment' if 'SCM_DO_BUILD_DURING_DEPLOYMENT' in os.environ else 'TechSupportSystem.settings'
+settings_module = "TechSupportSystem.settings"
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
