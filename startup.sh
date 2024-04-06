09ce0097534d721f4d4e15325ff47f3f39761fa7
BellaStefanova <bellastefanova7@gmail.com>
2024-04-06 09:53:26 +0300

    changes

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
-# set -o errexit
-# sed -i -e 's/\r$//' '/opt/startup/startup.sh'
-# source antenv/bin/activate
+#!/bin/sh
+
+echo "Start sshd"
+/usr/sbin/sshd
 python manage.py qcluster &
 
 gunicorn --bind=0.0.0.0 --timeout 600 TechSupportSystem.wsgi --access-logfile '-' --error-logfile '-'
