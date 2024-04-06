22ea7eb389177c264bb416c8a04fd2660af03ec6
BellaStefanova <bellastefanova7@gmail.com>
2024-04-06 13:25:05 +0300

    changes

--- TechSupportSystem/wsgi.py
+++ TechSupportSystem/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = "TechSupportSystem.deployment" if 'WEBSITE_HOSTNAME' in os.environ else 'TechSupportSystem.settings'
+settings_module = "TechSupportSystem.deployment" if 'DBHOST' in os.environ else 'TechSupportSystem.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
