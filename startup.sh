b65735ff74edb5680ab37e434c13334e3f0e6f43
BellaStefanova <bellastefanova7@gmail.com>
2024-04-06 09:19:16 +0300

    changed startup

--- startup.sh
+++ startup.sh
@@ -1,17 +1,7 @@
-# Set-AzWebApp -ResourceGroupName <group-name> -Name <app-name> -AppSettings @{"WEBSITES_PORT"="8000"}
+set -o errexit
+sed -i -e 's/\r$//' '/opt/startup/startup.sh'
+source antenv/bin/activate
 python manage.py qcluster &
+
 gunicorn --bind=0.0.0.0 --timeout 600 TechSupportSystem.wsgi --access-logfile '-' --error-logfile '-'
 
-# #!/usr/bin/env bash
-# # Exit on error
-# set -o errexit
-
-# # Modify this line as needed for your package manager (pip, poetry, etc.)
-# pip install -r requirements.txt
-
-# # Convert static asset files
-# python manage.py collectstatic --no-input
-
-# # Apply any outstanding database migrations
-
-# python manage.py migrate
