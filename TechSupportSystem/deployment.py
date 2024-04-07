bfc07f4db770d652bbfead49343c77b10e667790
BellaStefanova <bellastefanova7@gmail.com>
2024-04-07 10:45:21 +0300

    testing

--- TechSupportSystem/deployment.py
+++ TechSupportSystem/deployment.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], '*']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 SECURE_SSL_REDIRECT=0
