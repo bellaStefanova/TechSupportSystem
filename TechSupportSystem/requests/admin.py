5da1f9ecef9515041450b178f8c06e4c98017b92
BellaStefanova <bellastefanova7@gmail.com>
2024-04-10 10:34:26 +0300

    Adding Request to the django admin page

--- TechSupportSystem/requests/admin.py
+++ TechSupportSystem/requests/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Request
 
-# Register your models here.
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'urgency', 'status', 'user', 'last_updated_by']
+    list_filter = ['status', 'urgency', 'user']
